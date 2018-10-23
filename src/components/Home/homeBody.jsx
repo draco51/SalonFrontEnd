619d7cfe90d39326c461fa410dd8e4327c7a201b
Ishara Dhananjaya <danis994@gmail.com>
2018-10-23 14:51:06 +0530

    primary search loading data from API realtime

--- src/components/Home/homeBody.jsx
+++ src/components/Home/homeBody.jsx
@@ -2,53 +2,39 @@ import React, { Component } from "react";
 import AdBox from "./../AdBox/AdBox";
 import PrimarySearch from "../Search/primarySearch/primarySearch";
 import Feedback from "./feedback/feedback";
+import loadingGIF from "./../../img/loading.gif";
 
 class HomeBody extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
-      isLoaded: false,
+      isLoading: false,
       filteredItems: [],
       proURL: "",
-      alertMsg: ""
+      alertMsg: "",
+      updatedList: []
     };
   }
 
   //Search term input filtering
   HandleChange = event => {
-    console.log(event);
-    var updatedList = this.state.items;
-    updatedList = updatedList.filter(function(item) {
-      return (
-        item.name
-          .toString()
-          .toLowerCase()
-          .search(event.target.value.toString().toLowerCase()) !== -1 ||
-        item.location
-          .toString()
-          .toLowerCase()
-          .search(event.target.value.toString().toLowerCase()) !== -1 ||
-        item.profileTitle
-          .toString()
-          .toLowerCase()
-          .search(event.target.value.toString().toLowerCase()) !== -1
-      );
-    });
-    this.setState({ filteredItems: updatedList, alertMsg: "No results found" });
-  };
-
-  //Fetching freelancer profiles from API
-  componentDidMount() {
-    fetch(`${process.env.REACT_APP_API_URL}api/stylists`)
+    this.setState({ filteredItems: [], isLoading: true });
+    console.log(event.target.value);
+    const searchTerm = encodeURIComponent(event.target.value.toString());
+    fetch(
+      `${process.env.REACT_APP_API_URL}search_primary?searchTerm=${searchTerm}`
+    )
       .then(res => res.json())
       .then(json => {
         this.setState({
-          isLoaded: true,
-          items: json
+          isLoading: false,
+          filteredItems: json,
+          alertMsg: "No results found"
         });
       });
-  }
+    console.log(this.state.updatedList);
+  };
 
   //Rendering component
   render() {
@@ -59,7 +45,19 @@ class HomeBody extends Component {
           Find your perfect hair stylist and book them now.
         </div>
         <PrimarySearch HandleChange={this.HandleChange} id="primarySearchbox" />
-
+        {this.state.isLoading ? (
+          <div class="text-center">
+            <img
+              src={loadingGIF}
+              style={{
+                maxWidth: "70%",
+                height: "auto"
+              }}
+            />
+          </div>
+        ) : (
+          ""
+        )}
         {this.state.filteredItems.length > 0 ? (
           <div>
             {this.state.filteredItems.map(item => (
@@ -95,3 +93,40 @@ class HomeBody extends Component {
 }
 
 export default HomeBody;
+
+//----------------------Instant filter from item list-----------------------------------------------------------------------------------//
+// //Search term input filtering
+// HandleChange = event => {
+//   console.log(event);
+//   var updatedList = this.state.items;
+//   updatedList = updatedList.filter(function(item) {
+//     return (
+//       item.name
+//         .toString()
+//         .toLowerCase()
+//         .search(event.target.value.toString().toLowerCase()) !== -1 ||
+//       item.location
+//         .toString()
+//         .toLowerCase()
+//         .search(event.target.value.toString().toLowerCase()) !== -1 ||
+//       item.profileTitle
+//         .toString()
+//         .toLowerCase()
+//         .search(event.target.value.toString().toLowerCase()) !== -1
+//     );
+//   });
+//   this.setState({ filteredItems: updatedList, alertMsg: "No results found" });
+// };
+
+// //Fetching freelancer profiles from API
+// componentDidMount() {
+//   fetch(`${process.env.REACT_APP_API_URL}api/stylists`)
+//     .then(res => res.json())
+//     .then(json => {
+//       this.setState({
+//         isLoaded: true,
+//         items: json
+//       });
+//     });
+// }
+//---------------------------------------------------------------------------------------------------------//
